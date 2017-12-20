fde1b34afe53c3e50796515e8388826c817b5a83
YubaoLiu <yubaoliu89@gmail.com>
2017-12-20 18:30:55 +0800

    USB detection for WPF

---
+++ USBdetection/USBdetection/Views/Start.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace USBdetection.Views
+{
+  /// <summary>
+  /// Interaction logic for Start
+  /// </summary>
+  public partial class Start : UserControl
+  {
+    public Start()
+    {
+      InitializeComponent();
+    }
+  }
+}
