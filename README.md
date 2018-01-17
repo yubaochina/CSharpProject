ab36e00d3bb16f0d905c625bcd35444059352b7d
YubaoLiu <yubaoliu89@gmail.com>
2018-01-17 16:53:39 +0800

    Template project for Windows Service Deploy using C#

---
+++ README.md
@@ -0,0 +1,4 @@
+# Directory Introduction
+## WindowsService
+### Example-WindowsService
+A template project for Windows Service Installer using C#
---
+++ WindowsService/Example-WindowsService/Example-WindowsService/Service1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_WindowsService
+{
+  public partial class Service1 : ServiceBase
+  {
+    public Service1()
+    {
+      InitializeComponent();
+    }
+    public void OnDebug()
+    {
+      OnStart(null);
+    }
+
+    protected override void OnStart(string[] args)
+    {
+    //  System.IO.File.Create(Environment.CurrentDirectory + "OnStart.txt"); //System32
+      System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+    }
+
+    protected override void OnStop()
+    {
+      System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStop.txt");
+    }
+  }
+}
